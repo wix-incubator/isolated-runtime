f7cbd411dd9d9ea423cb69c9dff127d66d3ff700
Idan Attias <idana@wix.com>
2019-05-01 17:50:46 +0300

    update core d.ts file

--- packages/isolated-runtime-core/index.d.ts
+++ packages/isolated-runtime-core/index.d.ts
@@ -1,18 +1,25 @@
 type CodeRunnerOptions = {
   root: string;
   file: string;
-  onConsole: (method: string, ...args: any[]) => void;
-  sourceExtensions: string[];
-  compiler: (code: string) => string;
-  external: string[];
-  whitelistedPaths: string[];
-  resolve: (moduleName: string) => string;
-  resolveArguments: (args: any[]) => any[];
+  onConsole?: (method: string, ...args: any[]) => void;
+  sourceExtensions?: string[];
+  compiler?: (code: string) => string;
+  external?: string[];
+  whitelistedPaths?: string[];
+  resolve?: (moduleName: string) => string;
 };
+
+type RunOptions = {
+  funcName: string;
+  args: any[];
+  context?: object;
+  running?: () => void;
+  resolveArguments?: (args: any[]) => any[];
+}
 
 export class CodeRunner {
   constructor(opts: CodeRunnerOptions);
-  run(): Promise<any>;
+  run(opts: RunOptions): Promise<any>;
 }
 export namespace errors {
   export class ModuleNotFound {}
