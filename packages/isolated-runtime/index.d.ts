f4725d898db81b4fceaeba99e844f8c12f9d628c
Idan Attias <idana@wix.com>
2019-05-02 14:28:01 +0300

    update isolated-runtime d.ts

--- packages/isolated-runtime/index.d.ts
+++ packages/isolated-runtime/index.d.ts
@@ -1,21 +1,21 @@
 import { Options } from "generic-pool";
 
 type IsolatedRuntimeOptions = {
-  poolOptions: Options;
-  sourceExtensions: string[];
-  resolverModulePath: string;
-  compilerModulePath: string;
-  timeout: number;
+  poolOptions?: Options;
+  sourceExtensions?: string[];
+  resolverModulePath?: string;
+  compilerModulePath?: string;
+  timeout?: number;
 }
 type RunOptions = {
   root: string;
   file: string;
   funcName: string;
   args: any[];
-  context: object;
-  external: string[];
-  whitelistedPaths: string[];
-  resolverOptions: object;
+  context?: object;
+  external?: string[];
+  whitelistedPaths?: string[];
+  resolverOptions?: object;
 }
 
 export class IsolatedRuntime {
